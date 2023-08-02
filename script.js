7bc3e117ea0e5964fd9abb78797e280d7b7c3b19
vinayancha97 <135926111+vinayancha97@users.noreply.github.com>
2023-08-02 22:00:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-//your JS code here. If required.
-let select = document.getElementById("colorSelect");
-let btn =  document.querySelector("input[type=button]")
+const colorSelect = document.getElementById('colorSelect');
+  const removeButton = document.querySelector('input[type="button"]');
 
-btn.addEventlistener("click",()=>{
-	for (const child of select.children) {
-		if(child.value === select.value){
-			 child.remove();
-		}
-		
-	}
-});
+  removeButton.addEventListener('click', () => {
+    // Get the index of the selected option
+    const selectedIndex = colorSelect.selectedIndex;
+
+    // Check if an option is selected
+    if (selectedIndex !== -1) {
+      // Remove the selected option from the dropdown
+      colorSelect.remove(selectedIndex);
+    }
+  });
