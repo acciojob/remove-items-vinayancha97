7a8471f407975405267c876717d62e07adb09525
vinayancha97 <135926111+vinayancha97@users.noreply.github.com>
2023-08-02 21:58:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let select = document.getElementById("colorSelect");
+let btn =  document.querySelector("input[type=button]")
+
+btn.addEventlistener("click",()=>{
+	for (const child of select.children) {
+		if(child.value === select.value){
+			 child.remove();
+		}
+		
+	}
+});
